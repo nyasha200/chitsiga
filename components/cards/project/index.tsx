4db5e2aa5ed4aa2290059cd02e27683828b4ca79
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-11-11 09:39:41 +0200

    create card components

---
+++ components/cards/project/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectCard = () => {
+    return (
+        <div>
+            Project card
+        </div>
+    )
+}
+
+export default ProjectCard
---
+++ components/cards/testimonial/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonialCard = () => {
+    return (
+        <div>
+            testimonial card
+        </div>
+    )
+}
+
+export default TestimonialCard
