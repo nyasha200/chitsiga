e5add0504261cac4929e5e444d9bf4d0d6bd165b
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-11-23 12:42:12 +0200

    update privacy and terms pages

--- pages/privacy/index.tsx
+++ pages/privacy/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import Container from '@/components/ui-components/container';
+import RichText from '@/components/ui-components/rich-text';
+import apolloClient from '@/lib/apolloclient';
+import { GET_PAGE_DATA } from '@/lib/queries';
 
-const Privacy = () => {
+
+
+const Privacy = ({ item }: { item: { title: string; slug: string; details: JSON } | null }) => {
     return (
-        <div className='min-h-[80vh] '>
-            Privacy policy
+        <div className="flex justify-center min-h-[78vh]">
+            <Container className="w-full md:w-2/3">
+                <RichText content={item?.details} />
+            </Container>
         </div>
-    )
-}
+    );
+};
 
-export default Privacy
+export default Privacy;
+
+export async function getServerSideProps() {
+    try {
+        const { data } = await apolloClient.query({
+            query: GET_PAGE_DATA,
+            variables: { slug: 'privacy' },
+        });
+
+        const fetchedItem = data.pageCollection.items[0] || null;
+
+        return {
+            props: {
+                item: fetchedItem,
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching page data:', error);
+        return {
+            props: {
+                item: null,
+            },
+        };
+    }
+}
--- pages/terms/index.tsx
+++ pages/terms/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import Container from '@/components/ui-components/container';
+import RichText from '@/components/ui-components/rich-text';
+import apolloClient from '@/lib/apolloclient';
+import { GET_PAGE_DATA } from '@/lib/queries';
 
-const Terms = () => {
+
+
+const Terms = ({ item }: { item: { title: string; slug: string; details: JSON } | null }) => {
     return (
-        <div className='min-h-[80vh] '>
-            Terms and Conditions page
+        <div className="flex justify-center min-h-[78vh]">
+            <Container className="w-full md:w-2/3">
+                <RichText content={item?.details} />
+            </Container>
         </div>
-    )
-}
+    );
+};
 
-export default Terms
+export default Terms;
+
+export async function getServerSideProps() {
+    try {
+        const { data } = await apolloClient.query({
+            query: GET_PAGE_DATA,
+            variables: { slug: 'terms' },
+        });
+
+        const fetchedItem = data.pageCollection.items[0] || null;
+
+        return {
+            props: {
+                item: fetchedItem,
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching page data:', error);
+        return {
+            props: {
+                item: null,
+            },
+        };
+    }
+}
