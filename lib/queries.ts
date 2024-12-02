8289530cd9773daf896247004cc1c3209dd242a6
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-12-02 17:40:44 +0200

    update homepage data query

--- lib/queries.ts
+++ lib/queries.ts
@@ -13,3 +13,58 @@ export const GET_PAGE_DATA = gql`
     }
   }
 `;
+
+export const GET_HOMEPAGE_DATA = gql`
+  query GetHomepageData {
+    bannerItemCollection(limit: 5) {
+      items {
+        title
+        project {
+          title
+          slug
+          coverImage {
+            url
+          }
+        }
+      }
+    }
+    cardCollection(where: { type: "testimony" }) {
+      items {
+        type
+        title
+        description
+        image {
+          url
+        }
+      }
+    }
+    completedProjects: projectCollection(
+      order: sys_publishedAt_DESC
+      where: { stage: "completed" }
+      limit: 3
+    ) {
+      items {
+        title
+        stage
+        slug
+        coverImage {
+          url
+        }
+      }
+    }
+    underConstructionProjects: projectCollection(
+      order: sys_publishedAt_DESC
+      where: { stage: "construction" }
+      limit: 2
+    ) {
+      items {
+        title
+        slug
+        stage
+        coverImage {
+          url
+        }
+      }
+    }
+  }
+`;
