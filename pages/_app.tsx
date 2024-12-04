15c48c3dfe50f52b280b3bdad1d07a643b2aa743
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-12-04 12:47:15 +0200

    add raleway font to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,18 @@ import Header from "@/components/header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-raleway',
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
+  return <div className={raleway.variable}>
     <Header />
     <Component {...pageProps} />
     <Footer />
-  </>;
+  </div>;
 }
